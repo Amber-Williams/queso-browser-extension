fff00fe5f2f713f3628f29b9bcc6e346be90b4d5
amber williams <amberwilliamsdev@gmail.com>
2025-05-19 20:33:53 +0100

    fix: app icon compiled from zip

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,9 +7,7 @@ const isDev = process.env.NODE_ENV == 'development'
 const { version } = packageJson
 
 // Convert from Semver (example: 0.1.0-beta6)
-const [major, minor, patch] = version
-  .replace(/[^\d.-]/g, '')
-  .split(/[.-]/)
+const [major, minor, patch] = version.replace(/[^\d.-]/g, '').split(/[.-]/)
 
 export default defineManifest(async (env) => ({
   manifest_version: 3,
@@ -17,18 +15,13 @@ export default defineManifest(async (env) => ({
   version: `${major}.${minor}.${patch}`,
   description: packageJson.description,
   icons: {
-    '16': 'img/logo16.png',
-    '32': 'img/logo32.png',
-    '48': 'img/logo48.png',
-    '128': 'img/logo128.png',
+    16: 'img/logo-16.png',
+    32: 'img/logo-32.png',
+    48: 'img/logo-48.png',
+    128: 'img/logo-128.png',
   },
   action: {
-    default_icon: {
-      '16': 'img/logo16.png',
-      '32': 'img/logo32.png',
-      '48': 'img/logo48.png',
-      '128': 'img/logo128.png',
-    },
+    default_icon: 'img/logo-48.png',
     default_popup: 'popup.html',
   },
   side_panel: {
